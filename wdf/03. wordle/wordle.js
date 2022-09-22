11a9ac9722042f30118ce75cbb5a5e6dfcb976c2
KH <4491818+kiranjholla@users.noreply.github.com>
2022-09-22 08:51:08 +0530

    Some clean-up. Adding comments, fixing styles. Removed the word "Concept" from the title.

--- wdf/03. wordle/wordle.js
+++ wdf/03. wordle/wordle.js
@@ -6,6 +6,15 @@ const allowedKeys = ['Backspace', 'Enter'];
 const messageEl = document.getElementById('message-content');
 const spinnerEl = document.getElementById('spinner');
 
+/**
+ * handleKeyup() : Event handler for key-up event to check the character
+ * entered by the user and take appropriate action based on the type of
+ * character that has been entered.
+ *
+ * Only English alphabets, Backspace key, and Enter key are supported.
+ *
+ * @param {Event} event
+ */
 function handleKeyup(event) {
   if ((/^[a-zA-Z]$/.test(event.key) || allowedKeys.includes(event.code)) && activeRow <= 6) {
     switch (event.code) {
@@ -31,6 +40,11 @@ function handleKeyup(event) {
   }
 }
 
+/**
+ * showMessage() : Utility function to show any toast message
+ *
+ * @param {string} message
+ */
 function showMessage(message) {
   messageEl.innerText = message;
   messageEl.classList.remove('hidden');
@@ -39,6 +53,15 @@ function showMessage(message) {
   }, 3000);
 }
 
+/**
+ * markCharPositions() : Function is responsible for checking each character
+ * tile and marking whether the tile is green, yellow, or grey based on the
+ * character chosen in that position.
+ *
+ * This function also adds the 'flip' class which provides the tile-flip
+ * animation.
+ *
+ */
 function markCharPositions() {
   for (let i = 1; i < 6; i++) {
     document.getElementById(`r${activeRow}c${i}-front`).classList.add('flip');
@@ -48,6 +71,12 @@ function markCharPositions() {
   activeColumn = 1;
 }
 
+/**
+ * validateEnteredWord() : This function is responsible for checking the entered
+ * word-guess to verify if it is a valid word, and then invoke appropriate follow-up
+ * action -- either to show a message for invalid words, or to mark the char positions
+ * for valid words.
+ */
 function validateEnteredWord() {
   let word = '';
   for (let i = 1; i < 6; i++) {
@@ -84,10 +113,16 @@ function validateEnteredWord() {
     });
 }
 
+/**
+ * startSpinner() : Displays the spinner animation
+ */
 function startSpinner() {
   spinnerEl.classList.add('spinning');
 }
 
+/**
+ * stopSpinner() : Hides the spinner animation
+ */
 function stopSpinner() {
   spinnerEl.classList.remove('spinning');
 }
